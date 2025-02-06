b4351e20e77f944e17ceac6d22c8982ee2f9125f
Shama <shamaulsirin10@gmail.com>
2025-02-06 11:44:13 +0530

    new

---
+++ Day 4/Sqrt.java
@@ -0,0 +1,17 @@
+
+public class Sqrt {
+
+  public static void main(String[] args) {
+    int n=16;
+    int div=2;
+    
+    while(div<=n/2)
+    {
+      if(n/div==div){
+        System.out.println(div);
+        break;
+      }
+      div++;
+    }
+  }
+}
---
+++ Day 4/SumofDigits.java
@@ -0,0 +1,29 @@
+public class SumofDigits {
+
+  public static void main(String[] args) {
+    int n=12345;
+    int sum=sumofDigit(n);
+
+while(sum>9)
+{
+  sum=sumofDigit(sum);
+}
+System.out.println(sum);
+  }
+
+    public static int sumofDigit(int n)
+    {
+      int sum=0;
+      while(n!=0)
+      {
+        int rem=n%10;
+        sum+=rem;
+        n=n/10;
+      }
+      return sum; 
+
+    
+    
+    
+  }
+}
