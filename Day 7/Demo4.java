852faec805f8074d73f623cf27c6141c4261b1eb
Shama <shamaulsirin10@gmail.com>
2025-02-10 10:12:02 +0530

    day 7

---
+++ Day 7/Demo4.java
@@ -0,0 +1,13 @@
+public class Demo4 {
+  public static void main(String[] args) {
+    String s1="hello";
+    System.out.println(s1.contentEquals("hello"));
+
+    String s2="abc";
+    System.out.println(s2.equals("abc"));
+
+    String s3="this is my day 7 class learning string methods";
+    System.out.println(s3.contains("class"));
+  }
+  
+}
